16e46a77763eaaf968557829937c3b1a14237210
Lengyel Zsolt <zsolt.lengyel21@gmail.com>
2018-02-14 14:57:30 +0100

    javitas

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 /*jslint devel: true */
+/*global $, jQuery, alert*/
+/*jslint indent: 2 */
 
-console.log("hello world");
+
+(function () {
+  'use strict';
+  console.log("hello world");
+  $('.image-popup').magnificPopup({
+    type: 'image'
+  });
+}());
+
+
+
+
 
