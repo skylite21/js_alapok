85da7816fbe388aacf732a313f9c066852a5e0e9
skylite <skylite@macprompt.local>
2019-03-14 09:48:40 +0100

    03_14_2019_09_48_40 fast commit

--- basics/callback.js
+++ basics/callback.js
@@ -1,15 +1,27 @@
 let users = ['sam', 'john', 'peters'];
 
+
+// a setTimeout fügvény két paraméterrel dolgozik, 
+// az első az egy fügvény, a másik pedig egy szám...
+// az adott fügvényt futtatja le a megadott időn belül
+
+// pl az addUser fügvény úgy fog működni hogy a users tömbbe 200 milisecundum
+// után beteszi az új elemet (a username bemeneti paraméterbe amit megkap)
+
 // function addUser(username) {
 //   setTimeout( () => { users.push(username); 
 //   }, 200 );
 // }
-//
+
+// hasonlóan ez a fügvény pl kiírja a users tömb tartalmát de 100milisecundum késleltetéssel:
 // function getUsers() {
 //   setTimeout( () => { console.log(users);
 //   }, 100 );
 // }
 //
+
+// az alábbi két fügvény tehát fordított sorrendbe fog történni mert az addUser-es fügvényen belül
+// több a késleltetés:
 // addUser('Jim');
 //
 // getUsers();
